fd4ed5aa8a622a2cece4390facf17cb9f796d631
Linh <Linh@Linh-PC>
2012-07-01 13:19:28 +0700

    intilization

--- Smart Music/src/com/vinova/smart/music/HomeAcitivity.java
+++ Smart Music/src/com/vinova/smart/music/HomeAcitivity.java
@@ -1,23 +1,43 @@
 package com.vinova.smart.music;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
-public class HomeAcitivity extends Activity {
+public class HomeAcitivity extends Activity implements OnClickListener 
+{
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        initialization(savedInstanceState);
     }
-
+    
+    public void initialization(Bundle savedInstanceState)
+    {
+    	ImageButton songsMap = (ImageButton) findViewById(R.id.songsmap);
+        songsMap.setOnClickListener(this);
+        ImageButton albumsMap = (ImageButton) findViewById(R.id.albumsmap);
+        albumsMap.setOnClickListener(this);
+        ImageButton artistsMap = (ImageButton) findViewById(R.id.artistsmap);
+        artistsMap.setOnClickListener(this);
+        ImageButton genresMap = (ImageButton) findViewById(R.id.genresmap);
+        genresMap.setOnClickListener(this);
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_home, menu);
         return true;
     }
+
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		
+	}
     
 }
